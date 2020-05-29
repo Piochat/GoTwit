4832a285e462bb85182e8fbffab9ef0845920095
Kok Gudiel <kokegudiel2@gmail.com>
2020-05-29 17:37:20 -0600

    cambio ligeron main

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"log"
+
+	"github.com/Piochat/GoTwit/handlers"
+
+	"github.com/Piochat/GoTwit/db"
+)
+
 func main() {
-	println("Hola mundo")
+	if !db.CheckConnetcion() {
+		log.Fatalln("Error en la conexión de la base de datos")
+		return
+	}
+
+	handlers.Controllers()
 }
